be00790cbb9e6a62c996e307364cfc26f8ba56f7
Thiago Reis <thiagosbf@gmail.com>
2018-04-17 21:35:54 -0300

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+import os
+
+from omneek_api import __version__
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(here, 'README.md')) as f:
+    README = f.read()
+
+requires = ['requests']
+
+setup(
+    name='python-omneek-api',
+    version=__version__,
+    description='This library is a simple python wrapper for Omneek API',
+    long_description=README,
+    classifiers=[
+        "Programming Language :: Python",
+        "License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)",
+    ],
+    url='https://github.com/petertodd/python-bitcoinlib',
+    keywords='omneek',
+    packages=find_packages(),
+    zip_safe=False,
+    install_requires=requires,
+    test_suite="omneek_api.tests"
+)
